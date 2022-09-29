c290a7866aae6a109f769138fa961100fa6c0ce3
vitoros9x <anhkhoa9xhn@gmail.com>
2022-09-30 00:12:16 +0700

    update

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -1,27 +1,25 @@
-import Tippy from '@tippyjs/react/headless'
+import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
-import styles from "./Menu.module.scss"
+import styles from './Menu.module.scss';
 
-import { Wrapper as PopperWrapper } from '~/components/Popper'
+import { Wrapper as PopperWrapper } from '~/components/Popper';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function Menu({ children }) {
   return (
     <Tippy
-      visible
+      placement="bottom-end"
       interactive
       render={(attrs) => (
-        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-          <PopperWrapper>
-
-          </PopperWrapper>
+        <div className={cx('content')} tabIndex="-1" {...attrs}>
+          <PopperWrapper>Menu</PopperWrapper>
         </div>
       )}
     >
       {children}
     </Tippy>
-  )
+  );
 }
 
 export default Menu;
